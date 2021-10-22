bee8505bbd81d611b8e8b58a33c9fa3f3d20982c
Neesh Samsi <avaneeshsamsi@gmail.com>
2021-10-22 12:27:19 +0530

    feat: imported icons

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,5 +1,9 @@
 import React from "react";
 
+// Icons
+import { BsPlus, BsFillLightningFill, BsGearFill } from "react-icons/bs";
+import { FaFire, FaPoo } from "react-icons/fa";
+
 const SideBar = () => {
   return (
     <div className="fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-gray-900 text-white shadow-lg">
